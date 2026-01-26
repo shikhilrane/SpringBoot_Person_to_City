33ffe6f2fa76afb0739b99b8d31add27c086bacf
Shikhil Rane <shikhilrane04@gmail.com>
2026-01-26 14:05:27 +0530

    first commit

---
+++ src/main/java/com/shikhilrane/personCity/personToCity/PersonToCityApplication.java
@@ -0,0 +1,13 @@
+package com.shikhilrane.personCity.personToCity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonToCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonToCityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shikhilrane/personCity/personToCity/PersonToCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shikhilrane.personCity.personToCity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonToCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
